8a88a55caabe43ed33e4eb5e1a47dc48d734521a
BJIACTb <50963370+BJIACTb@users.noreply.github.com>
2022-05-05 23:15:41 +0600

    +api

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,78 +1,221 @@
-import React from "react"
-import "./Main.css"
-function Main() {
- return <main className="Main"> 
+import axios, * as others from "https://cdn.skypack.dev/axios@0.21.1";
+import ReactPaginate from "https://cdn.skypack.dev/react-paginate@7.1.0";
+import React from "react";
+import "./Main.scss"
+// https://www.omdbapi.com/?apikey=756abb2f&i=tt4154664&plot=full
 
-    <div className="spisokfilm"> 
-    <img src="https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/731c4031-7389-44f4-8c15-f9f4e3b0ed90/300x450"/>
-                <div class="filmhover">
-                    <h2>Человек паук</h2>
-                </div>
+const { useState, useEffect } = React;
+
+// Main component -------------------------------
+const Main = (props) => {
+  
+  const getMovies = (term='war', page=1) => {
+    setLoading(true);
+    axios.get(`https://www.omdbapi.com/?apikey=756abb2f&s=${encodeURIComponent(term)}&plot=full&page=${page}`)
+      .then(response => {
+      // console.log(response.data);
+      setMovies(response.data.Search);
+      setTotal(Math.ceil(response.data.totalResults/10))
+      setPages(page);
+      setLoading(false);
+    }).catch(error => {
+      // console.log(error);
+      setMovies([]);
+      setLoading(false);
+    });
+  }
+  
+  const getMovie = (movieId) => {
+    axios.get(`https://www.omdbapi.com/?apikey=756abb2f&i=${movieId}&plot=full`)
+    .then(response => {
+      // console.log(response.data);
+      setMovie(response.data);
+      setShowPop(true);
+    });
+  }
+  
+  const [loading, setLoading] = useState(true);
+  const [showPop, setShowPop] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState({});
+  const [keyword, setKeyword] = useState('frozen');
+  const [pages, setPages] = useState(1);
+  const [total, setTotal] = useState();
+  useEffect(() => {
+    getMovies();
+  }, []);
+  
+  const handlePageClick = e => {
+    console.log(e.selected);
+    getMovies(keyword, (e.selected + 1));
+  }
+  
+  const handleCardClicked = movieId => {
+    // console.log(movieId);
+    getMovie(movieId);
+    
+  }
+  
+  const genCards = () => {
+    let allCards = [];
+    if(movies){
+      movies.map(movie => {
+        allCards.push(<Card {...movie} cardClicked={handleCardClicked} />);
+      });
+    }
+    return allCards;
+  }
+  
+  let allCards = genCards();
+  
+  return(
+    <div className='main'>
+      <h1 className='head'>Movieflix</h1>
+      <Search 
+        getInputValue={val => setKeyword(val)} 
+        sendEnter={() => getMovies(keyword)} 
+        getSubmit={() => getMovies(keyword)} />
+      
+     { showPop ? <Showexpand {...movie} closePop={() => setShowPop(false)} /> : null }
+      
+      <div className='cards'>
+        { loading ? <Loader /> : 
+        (allCards.length === 0 ? 
+         <div className='error'>
+           No movie found...
+           <i class="far fa-grin-beam-sweat"></i>
+         </div> : allCards) }
+      </div>
+      
+      <div className='paginate'>
+        <ReactPaginate 
+          previousLabel={"Back"}
+          nextLabel={"Next"}
+          breakLabel={"..."}
+          breakClassName={"break-me"}
+          pageCount={total}
+          marginPagesDisplayed={1}
+          pageRangeDisplayed={4}
+          onPageChange={handlePageClick}
+          containerClassName={"pagination"}
+          subContainerClassName={"pages pagination"}
+          activeClassName={"active"}
+          />
+      </div>
+      
     </div>
+  )
+  
+}
 
 
 
-	<div className="spisokfilm"> 
-    <img src="https://upload.wikimedia.org/wikipedia/ru/f/f0/Green_Book.jpg"/>
-                <div class="filmhover">
-                    <h2>Зеленая книга</h2>
-                </div>
+// Loader component -------------------------------
+const Loader = props => {
+  
+  return (
+     <div className='loader'>
+      Loading...
+      <i class="fas fa-sync-alt fa-spin"></i>
     </div>
+  )
+}
 
 
-	<div className="spisokfilm"> 
-    <img src="https://upload.wikimedia.org/wikipedia/ru/0/05/Venom_poster.jpg"/>
-                <div class="filmhover">
-                    <h2>Веном</h2>
-                </div>
+// Search component -------------------------
+const Search = (props) => {
+
+  let sendValue = (e) => {
+    props.getInputValue(e.target.value)
+  }
+  
+  const handleSubmit = () => {
+    props.getSubmit();
+  }
+  
+  const handleKeyDown = (e) => {
+    if(e.key === 'Enter'){
+      props.sendEnter();
+      // console.log('Enter');
+    }
+  }
+  
+  return(
+    <div className='search'>
+      <div className='search-box'>
+        <input type='text'
+          onChange={sendValue}
+          onKeyDown={handleKeyDown}
+          placeholder='Find movie...' />
+        <button onClick={handleSubmit} ><i class="fas fa-search"></i></button>
+      </div>
     </div>
+  )
+}
 
-	<div className="spisokfilm"> 
-    <img src="https://upload.wikimedia.org/wikipedia/ru/thumb/8/8e/Venom_-_Let_There_Be_Carnage.jpg/200px-Venom_-_Let_There_Be_Carnage.jpg"/>
-                <div class="filmhover">
-                    <h2>Веном 2</h2>
-                </div>
+
+// Card component ------------------------------------------------
+const Card = props => {
+  
+  // console.log(props);
+  const handleClick = (e) => {
+    // console.log(e.target.dataset.id);
+    props.cardClicked(e.target.dataset.id);
+  }
+  return (
+    <div className='card' title={props.Title} data-id={props.imdbID} onClick={handleClick}>
+      <img src={props.Poster !== 'N/A' ? props.Poster : 'https://via.placeholder.com/163x240/111217/FFFFFF/?text=No%20Image'} alt={props.Title} data-id={props.imdbID} />
     </div>
+  )
+}
 
-	<div className="spisokfilm"> 
-    <img src="https://upload.wikimedia.org/wikipedia/ru/9/9d/Insidious-_The_Last_Key.jpg"/>
-                <div class="filmhover">
-                    <h2>Астрал 4</h2>
-                </div>
+
+
+// Show expand component ---------------------------------------
+const Showexpand = (props) => {
+  
+  console.log(props)
+  
+  return (
+    <div className='show-expand'>
+      
+      <div className='show-content'>
+        <i class="show-close fas fa-times" onClick={props.closePop} ></i>
+        
+        <div className='show-poster'>
+          <span className='show-poster-bg'>
+            <img src={props.Poster !== 'N/A' ? props.Poster : 'https://via.placeholder.com/163x240/111217/FFFFFF/?text=No%20Image'} alt={props.Title} />
+          </span>
+          <span className='show-poster-main'>
+            <img src={props.Poster !== 'N/A' ? props.Poster : 'https://via.placeholder.com/163x240/111217/FFFFFF/?text=No%20Image'} alt={props.Title} />
+          </span>
+        </div>
+        
+        <div className='show-detail'>
+          <h2>{props.Title}</h2>
+          <ul className="show-tags">
+            <li className="show-rated">{props.Country}</li>
+            <li className="show-rated">{props.Year}</li>
+            <li className="show-year">{props.Rated}</li>
+            <li className="show-year">{props.Genre}</li>
+          </ul>
+          <div class="show-plot">
+            <p>{props.Plot}</p>
+          </div>
+          
+          <div class="show-credits">
+            <p><strong>Production:</strong> {props.Production}</p>
+            <p><strong>Runtime:</strong> {props.Runtime || 'N/A '}</p>
+            <p><strong>Rating:</strong> {props.imdbRating}</p>
+            <p><strong>Director:</strong> {props.Director}</p>
+            <p><strong>Actors:</strong> {props.Actors}</p>
+            <p><strong>BoxOffice:</strong> {props.BoxOffice || 'N/A '}</p>
+          </div>
+        </div>
+        
+      </div>
     </div>
-
-
-	<div className="spisokfilm"> 
-    <img src="https://avatars.mds.yandex.net/get-kinopoisk-image/1773646/96d93e3a-fdbf-4b6f-b02d-2fc9c2648a18/600x900"/>
-                <div class="filmhover">
-                    <h2>Титаник</h2>
-                </div>
-                
-    </div>
-
-    <div className="spisokfilm"> 
-    <img src="https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/70c27684-81cd-49e1-bf39-250b26702864/600x900"/>
-                <div class="filmhover">
-                    <h2>Атака Титанов</h2>
-                </div>
-    </div>
-
-    <div className="spisokfilm"> 
-    <img src="https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/c11131ea-c6e0-4a0e-bdda-9009da1d8c30/600x900"/>
-                <div class="filmhover">
-                    <h2>Волейбол</h2>
-                </div>
-    </div>
-
-    <div className="spisokfilm"> 
-    <img src="https://avatars.mds.yandex.net/get-kinopoisk-image/4774061/16b28018-6065-4478-9835-3f2caae35582/600x900"/>
-                <div class="filmhover">
-                    <h2>Золушка</h2>
-                </div>
-    </div>
-
- </main>
- 
- }
+  )
+}
 
 export default Main;
